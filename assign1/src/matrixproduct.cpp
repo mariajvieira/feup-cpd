a18d8706151040651f566faf4dcafce66f423c9c
Duarte Manuel Gonçalves Marques <up202204973@edu.fe.up.pt>
2025-03-19 18:31:40 +0000

    Parallel Line Multiplication

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -94,15 +94,67 @@ void OnMultLine(int m_ar, int m_br)
 			pha[i*m_ar + j] = (double)1.0;
 
 
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+    Time1 = clock();
+
+	for (int i = 0; i < m_ar; i++) {
+		for (int k = 0; k < m_br; k++) {
+			for (int j = 0; j < m_br; j++) {
+				phc[i*m_ar + j] += pha[i*m_ar + k] * phb[k*m_br + j];
+			}
+		}
+	}
+	
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+	
+    
+}
+void ParallelOnMultLine(int m_ar, int m_br)
+{
+    SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
 
 	for(i=0; i<m_br; i++)
 		for(j=0; j<m_br; j++)
 			phb[i*m_br + j] = (double)(i+1);
 
 
-
     Time1 = clock();
-
+	# pragma omp parallel for
 	for (int i = 0; i < m_ar; i++) {
 		for (int k = 0; k < m_br; k++) {
 			for (int j = 0; j < m_br; j++) {
@@ -257,6 +309,7 @@ int main (int argc, char *argv[])
 		cout << endl << "1. Multiplication" << endl;
 		cout << "2. Line Multiplication" << endl;
 		cout << "3. Block Multiplication" << endl;
+		cout << "4. Parallel Line Multiplication" << endl;
 		cout << "Selection?: ";
 		cin >>op;
 		if (op == 0)
@@ -282,7 +335,9 @@ int main (int argc, char *argv[])
 				cin >> blockSize;
 				OnMultBlock(lin, col, blockSize);  
 				break;
-
+			case 4:
+				ParallelOnMultLine(lin, col);  
+				break;
 		}
 
   		ret = PAPI_stop(EventSet, values);
